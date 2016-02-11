68ff1fe0f8574eaf5771a4d07bd4dcb05439d73d
ACaiazza22 <amc539@scarletmail.rutgers.edu>
2016-02-11 12:54:40 -0500

    Tree movement

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -12,12 +12,22 @@ public class AppleTree : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+		if(pos.x < -leftAndRightEdge){
+			speed = Mathf.Abs(speed);
+
+		}else if(pos.x > leftAndRightEdge){
+			speed = -Mathf.Abs(speed);
+		}
 	
 	}
 }
